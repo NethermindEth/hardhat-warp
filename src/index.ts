02171436f3d715c3bdb096d6fbdf9dfd56ea7733
Glitch18 <atharvakn@gmail.com>
2022-07-25 19:21:58 +0530

    Set up eslint

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,24 @@
-import { Transpiler } from "./transpiler";
-import { task, types } from "hardhat/config";
+import {Transpiler} from './transpiler';
+import {task, types} from 'hardhat/config';
 
-task("transpile")
-  .addParam("contract", "Path to Solidity contract", undefined, types.string, false)
-  .addParam("warpPath", "Path to warp binary", undefined, types.string, false)
-  .setAction(
-    async ({
-      contract,
-      warpPath,
-    }: {
+task('transpile')
+    .addParam('contract', 'Path to Solidity contract', undefined, types.string, false)
+    .addParam('warpPath', 'Path to warp binary', undefined, types.string, false)
+    .setAction(
+        async ({
+          contract,
+          warpPath,
+        }: {
       contract: string;
       warpPath: string;
     }): Promise<string> => {
-      const transpiler = new Transpiler(warpPath);
+          const transpiler = new Transpiler(warpPath);
 
-      const result = await transpiler.transpile(contract);
+          const result = await transpiler.transpile(contract);
 
-      return result;
-    }
-  );
+          return result;
+        },
+    );
 
-task("test")
-  .setAction(async () => console.log("Yeah this is a test"));
+task('test')
+    .setAction(async () => console.log('Yeah this is a test'));
--- src/transpiler.ts
+++ src/transpiler.ts
@@ -1,26 +1,23 @@
-import { exec } from 'child_process';
+import {exec} from 'child_process';
 
 export class Transpiler {
-    constructor(private _pathToWarp: string) {}
+  constructor(private _pathToWarp: string) {}
 
-    /**
-     * @param inputPath path to contract to be compiled
-     */
-    public async transpile(inputPath: string) {
-        const output : string = await new Promise((resolve, reject) => {
-            console.log(`${this._pathToWarp} transpile ${inputPath}`);
-            const process = exec(
-                `${this._pathToWarp} transpile ${inputPath}`, 
-                (error, stdout) => {
-                    if (error !== null) return reject(error);
-                    resolve(stdout);
-                }
-            );
+  public async transpile(inputPath: string) {
+    const output : string = await new Promise((resolve, reject) => {
+      console.log(`${this._pathToWarp} transpile ${inputPath}`);
+      const process = exec(
+          `${this._pathToWarp} transpile ${inputPath}`,
+          (error, stdout) => {
+            if (error !== null) return reject(error);
+            resolve(stdout);
+          },
+      );
 
             process.stdin!.end();
-        })
+    });
 
-        console.log(output);
-        return output;
-    }    
+    console.log(output);
+    return output;
+  }
 }
