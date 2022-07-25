edc0ec2c01fa70770c56b9c613168863600cf455
Glitch18 <atharvakn@gmail.com>
2022-07-25 12:11:22 +0530

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import { Transpiler } from "./transpiler";
+import { task, types } from "hardhat/config";
+
+task("transpile")
+  .addParam("contract", "Path to Solidity contract", undefined, types.string, false)
+  .addParam("warpPath", "Path to warp binary", undefined, types.string, false)
+  .setAction(
+    async ({
+      contract,
+      warpPath,
+    }: {
+      contract: string;
+      warpPath: string;
+    }): Promise<string> => {
+      const transpiler = new Transpiler(warpPath);
+
+      const result = await transpiler.transpile(contract);
+
+      return result;
+    }
+  );
+
+task("test")
+  .setAction(async () => console.log("Yeah this is a test"));
