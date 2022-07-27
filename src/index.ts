bf480d640c59a0877506bca88ebdff805e8016ef
Glitch18 <atharvakn@gmail.com>
2022-07-27 16:47:48 +0530

    Implement TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
-import {Transpiler} from './transpiler';
+import {
+  TASK_COMPILE_GET_COMPILATION_TASKS, TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS,
+} from 'hardhat/builtin-tasks/task-names';
 import {subtask, types} from 'hardhat/config';
-import {TASK_COMPILE_GET_COMPILATION_TASKS}
-  from 'hardhat/builtin-tasks/task-names';
-import {TASK_COMPILE_WARP, TASK_COMPILE_WARP_RUN_BINARY} from './task-names';
+import {glob} from 'hardhat/internal/util/glob';
+import path from 'path';
+import {
+  TASK_COMPILE_WARP,
+  TASK_COMPILE_WARP_RUN_BINARY,
+} from './task-names';
+import {Transpiler} from './transpiler';
 
 subtask(
     TASK_COMPILE_GET_COMPILATION_TASKS,
@@ -12,6 +18,14 @@ subtask(
     },
 );
 
+subtask(TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS, undefined,
+    async (_, {config}): Promise<string[]> => {
+      const solPaths = await glob(path.join(config.paths.root, 'contracts/**/*.sol'));
+      const cairoPaths = await glob(path.join(config.paths.root, 'contracts/**/*.cairo.sol'));
+
+      return solPaths.filter((x) => !cairoPaths.includes(x));
+    },
+);
 subtask(TASK_COMPILE_WARP_RUN_BINARY)
     .addParam('contract', 'Path to Solidity contract', undefined, types.string, false)
     .addParam('warpPath', 'Path to warp binary', undefined, types.string, false)
