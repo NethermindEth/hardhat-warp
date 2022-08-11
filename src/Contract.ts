d543da9edd8fd0dadc05b2f2e8a58ffc26d1b95c
Glitch18 <atharvakn@gmail.com>
2022-08-11 06:31:51 +0530

    Implement getCompiledJson

--- src/Contract.ts
+++ src/Contract.ts
@@ -1,4 +1,5 @@
-export class Contract {
+import path from 'path';
+export class ContractInfo {
   private name: string;
   private solidityFile: string;
   private deployedAddress = '';
@@ -29,5 +30,8 @@ export class Contract {
     const cairoFile = this.solidityFile.slice(0, -4).replace('_', '__').concat(`__WC__${this.name}.cairo`);
     return path.join('warp_output', cairoFile);
   }
+
+  getCompiledJson() {
+    return this.getCairoFile().slice(0, -6).concat('_compiled.json');
   }
 }
