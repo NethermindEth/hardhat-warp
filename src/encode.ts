ccd8668ac548537a4691b19633e545954013119f
Swapnil Raj <raj.swapnil99@gmail.com>
2022-08-30 13:47:47 +0100

    Checkpoint decoding

--- src/encode.ts
+++ src/encode.ts
@@ -1,11 +1,33 @@
-import {BigNumberish} from "ethers";
-import BN from 'bn.js';
-import {ParamType, Result} from "ethers/lib/utils";
-import {AddressType, ArrayType, BoolType, BuiltinStructType, BuiltinType, BytesType, ContractDefinition, EnumDefinition, FixedBytesType, FunctionType, IntType, MappingType, PointerType, StringType, TypeNameType, TypeNode, UserDefinedType} from "solc-typed-ast";
+import { BigNumberish } from "ethers";
+import BN from "bn.js";
+import { ParamType, Result } from "ethers/lib/utils";
+import {
+  AddressType,
+  ArrayType,
+  BoolType,
+  BuiltinStructType,
+  BuiltinType,
+  BytesType,
+  ContractDefinition,
+  EnumDefinition,
+  FixedBytesType,
+  FunctionType,
+  IntType,
+  MappingType,
+  PointerType,
+  StringType,
+  TypeNameType,
+  TypeNode,
+  UserDefinedType,
+} from "solc-typed-ast";
 
-export type SolValue = string | SolValue[]
+export type SolValue = string | SolValue[];
 
-export function encodeValue(tp: TypeNode, value: SolValue, compilerVersion: string): string[] {
+export function encodeValue(
+  tp: TypeNode,
+  value: SolValue,
+  compilerVersion: string
+): string[] {
   if (tp instanceof IntType) {
     return encodeAsUintOrFelt(tp, value, tp.nBits);
   } else if (tp instanceof ArrayType) {
@@ -21,30 +43,30 @@ export function encodeValue(tp: TypeNode, value: SolValue, compilerVersion: stri
       return value.flatMap((v) => encodeValue(tp.elementT, v, compilerVersion));
     }
   } else if (tp instanceof BoolType) {
-    if (typeof value !== 'boolean') {
+    if (typeof value !== "boolean") {
       throw new Error(`Can't encode ${value} as boolType`);
     }
-    return [value ? '1' : '0'];
+    return [value ? "1" : "0"];
   } else if (tp instanceof BytesType) {
-    if (value === null) return ['0'];
-    if (typeof value !== 'string') {
+    if (value === null) return ["0"];
+    if (typeof value !== "string") {
       throw new Error(`Can't encode ${value} as bytesType`);
     }
     // removing 0x
     value = value.substring(2);
     const length = value.length / 2;
-    if (length !== Math.floor(length)) throw new Error('bytes must be even');
+    if (length !== Math.floor(length)) throw new Error("bytes must be even");
 
     const cairoBytes: string[] = [];
     for (let index = 0; index < value.length; index += 2) {
       const byte = value.substring(index, index + 2);
-      cairoBytes.push(BigInt('0x' + byte).toString());
+      cairoBytes.push(BigInt("0x" + byte).toString());
     }
     return [length.toString(), cairoBytes].flat();
   } else if (tp instanceof FixedBytesType) {
     return encodeAsUintOrFelt(tp, value, tp.size * 8);
   } else if (tp instanceof StringType) {
-    if (typeof value !== 'string') {
+    if (typeof value !== "string") {
       throw new Error(`Can't encode ${value} as stringType`);
     }
     const valueEncoded: number[] = Buffer.from(value).toJSON().data;
@@ -55,27 +77,35 @@ export function encodeValue(tp: TypeNode, value: SolValue, compilerVersion: stri
   } else if (tp instanceof AddressType) {
     return encodeAsUintOrFelt(tp, value, 160);
   } else if (tp instanceof BuiltinType) {
-    throw new Error('Serialising BuiltinType not supported yet');
+    throw new Error("Serialising BuiltinType not supported yet");
   } else if (tp instanceof BuiltinStructType) {
-    throw new Error('Serialising BuiltinStructType not supported yet');
+    throw new Error("Serialising BuiltinStructType not supported yet");
   } else if (tp instanceof MappingType) {
-    throw new Error('Mappings cannot be serialised as external function paramenters');
+    throw new Error(
+      "Mappings cannot be serialised as external function paramenters"
+    );
   } else if (tp instanceof UserDefinedType) {
-    throw new Error('UserDefinedType should not exist in raw abi');
+    throw new Error("UserDefinedType should not exist in raw abi");
   } else if (tp instanceof FunctionType) {
-    throw new Error('Serialising FunctionType not supported yet');
+    throw new Error("Serialising FunctionType not supported yet");
   } else if (tp instanceof PointerType) {
     return encodeValue(tp.to, value, compilerVersion);
   }
   throw new Error(`Don't know how to convert type ${printTypeNode(tp)}`);
 }
 
-export function encodeAsUintOrFelt(tp: TypeNode, value: SolValue, nBits: number): string[] {
-  if (typeof value !== 'string') {
+export function encodeAsUintOrFelt(
+  tp: TypeNode,
+  value: SolValue,
+  nBits: number
+): string[] {
+  if (typeof value !== "string") {
     throw new Error(`Can't encode ${value} as ${printTypeNode(tp)}`);
   }
   try {
-    return toUintOrFelt(BigInt(value.toString()), nBits).map((x) => x.toString());
+    return toUintOrFelt(BigInt(value.toString()), nBits).map((x) =>
+      x.toString()
+    );
   } catch {
     throw new Error(`Can't encode ${value} as ${printTypeNode(tp)}`);
   }
@@ -90,11 +120,13 @@ export function printTypeNode(node: TypeNode, detail?: boolean): string {
 }
 
 function printTypeNodeTypes(node: TypeNode): string {
-  let subTypes = '';
+  let subTypes = "";
   if (node instanceof ArrayType) {
     subTypes = `(${printTypeNodeTypes(node.elementT)}, ${node.size})`;
   } else if (node instanceof MappingType) {
-    subTypes = `(${printTypeNodeTypes(node.keyType)}, ${printTypeNodeTypes(node.valueType)})`;
+    subTypes = `(${printTypeNodeTypes(node.keyType)}, ${printTypeNodeTypes(
+      node.valueType
+    )})`;
   } else if (node instanceof PointerType) {
     subTypes = `(${printTypeNodeTypes(node.to)}, ${node.location})`;
   } else if (node instanceof TypeNameType) {
@@ -103,7 +135,7 @@ function printTypeNodeTypes(node: TypeNode): string {
   return `${node.constructor.name} ${subTypes}`;
 }
 
-const uint128 = BigInt('0x100000000000000000000000000000000');
+const uint128 = BigInt("0x100000000000000000000000000000000");
 
 export function toUintOrFelt(value: bigint, nBits: number): bigint[] {
   const val = bigintToTwosComplement(BigInt(value.toString()), nBits);
@@ -130,8 +162,12 @@ export function bigintToTwosComplement(val: bigint, width: number): bigint {
     // Negative values need to be converted to two's complement
     // This is done by flipping the bits, adding one, and truncating
     const absBits = (-val).toString(2);
-    const allBits = `${'0'.repeat(Math.max(width - absBits.length, 0))}${absBits}`;
-    const inverted = `0b${[...allBits].map((c) => (c === '0' ? '1' : '0')).join('')}`;
+    const allBits = `${"0".repeat(
+      Math.max(width - absBits.length, 0)
+    )}${absBits}`;
+    const inverted = `0b${[...allBits]
+      .map((c) => (c === "0" ? "1" : "0"))
+      .join("")}`;
     const twosComplement = (BigInt(inverted) + 1n).toString(2).slice(-width);
     return BigInt(`0b${twosComplement}`);
   }
@@ -139,10 +175,9 @@ export function bigintToTwosComplement(val: bigint, width: number): bigint {
 
 export function decode(
   types: ParamType[],
-  outputs: string[]
+  outputs: IterableIterator<[string]>
 ): Result {
-  const outputIterator = outputs.values()
-  return []
+  return [];
 }
 
 function decodePrimitive(
@@ -150,38 +185,48 @@ function decodePrimitive(
   outputs: IterableIterator<[string]>
 ): BigNumberish {
   if (typeString.startsWith("uint")) {
-    return decodeUint(typeString.length > 4 ? parseInt(typeString.slice(4)) : 256, outputs)
+    return decodeUint(
+      typeString.length > 4 ? parseInt(typeString.slice(4), 10) : 256,
+      outputs
+    );
   }
   if (typeString.startsWith("int")) {
-    return decodeInt(typeString.length > 3 ? parseInt(typeString.slice(3)) : 256, outputs)
+    return decodeInt(
+      typeString.length > 3 ? parseInt(typeString.slice(3), 10) : 256,
+      outputs
+    );
   }
   if (typeString === "address") {
-    return readFelt(outputs)
+    return readFelt(outputs);
   }
   if (typeString === "bool") {
-    return readFelt(outputs)
+    // TODO: may need to convert to true/false
+    return readFelt(outputs);
   }
   if (typeString === "fixed" || typeString === "ufixed") {
-    throw new Error("Not Supported")
+    throw new Error("Not Supported");
   }
   if (typeString.startsWith("bytes")) {
     return typeString.length === 5 ? decodeBytes(outputs) : decodeFixedBytes(outputs, parseInt(typeString.slice(5)))
   }
-  return 1n
+  return 1n;
 }
 
-function readFelt(outputs: IterableIterator<[string]>) : bigint {
-    return BigInt(outputs.next().value)
+function readFelt(outputs: IterableIterator<[string]>): bigint {
+  return BigInt(outputs.next().value);
 }
 
 function readUint(outputs: IterableIterator<[string]>): bigint {
-    const low = BigInt(outputs.next().value)
-    const high = BigInt(outputs.next().value)
-    return (high << 128n) + low;
+  const low = BigInt(outputs.next().value);
+  const high = BigInt(outputs.next().value);
+  return (high << 128n) + low;
 }
 
-function decodeUint(nbits: number, outputs: IterableIterator<[string]>): bigint {
-  return nbits < 256 ? readFelt(outputs) : readUint(outputs)
+function decodeUint(
+  nbits: number,
+  outputs: IterableIterator<[string]>
+): bigint {
+  return nbits < 256 ? readFelt(outputs) : readUint(outputs);
 }
 
 function decodeInt(nbits: number, outputs: IterableIterator<[string]>): bigint {
@@ -205,14 +250,77 @@ function decodeFixedBytes(outputs: IterableIterator<[string]>, length: number) {
 }
 
 export function twosComplementToBigInt(val: bigint, width: number): bigint {
-  const mask = 2n ** BigInt(width) - 1n
-  const max = 2n ** BigInt(width - 1) - 1n
+  const mask = 2n ** BigInt(width) - 1n;
+  const max = 2n ** BigInt(width - 1) - 1n;
   if (val > max) {
     // Negative number
-    const pos = (val ^ mask) + 1n
-    return -pos
+    const pos = (val ^ mask) + 1n;
+    return -pos;
   } else {
     // Positive numbers as are
-    return val
+    return val;
+  }
+}
+
+export function getWidthOf(type: ParamType): number {
+  if (type.baseType.startsWith("uint")) {
+    const width = parseInt(type.baseType.slice(4), 10);
+    return width < 256 ? 1 : 2;
+  } else if (type.baseType.startsWith("int")) {
+    const width = parseInt(type.baseType.slice(3), 10);
+    return width < 256 ? 1 : 2;
+  } else if (type.baseType.startsWith("address")) {
+    return 1;
+  } else if (type.baseType.startsWith("bool")) {
+    return 1;
+  } else if (/byte\d*$/.test(type.baseType)) {
+    const width = parseInt(type.baseType.slice(4), 10);
+    return width * 8;
+  } else if (
+    type.baseType.startsWith("fixed") ||
+    type.baseType.startsWith("fixed")
+  ) {
+    throw new Error("Not Supported");
+  } else if (type.baseType.startsWith("bytes")) {
+    throw new Error("Not supported dynamic in dynamic types");
+  } else if (type.indexed) {
+    // array
+    if (type.arrayLength === -1) {
+      throw new Error("Not supported dynamic in dynamic types");
+    } else {
+      // static array
+      return type.arrayLength * getWidthOf(type.arrayChildren);
+    }
+  } else if (type.components.length !== 0) {
+    // struct
+    return type.components.reduce((acc, ty) => {
+      return acc + getWidthOf(ty);
+    }, 0);
+  }
+  throw new Error("Not Supported " + type.baseType);
+}
+
+export function decodeComplex(
+  type: ParamType,
+  outputs: IterableIterator<[string]>
+) {
+  if (type.indexed) {
+    // array type
+    const length =
+      type.arrayLength === -1 ? readFelt(outputs) : type.arrayLength;
+    const result: Result = [];
+    for (let i = 0; i < length; ++i) {
+      result.push(decode([type.arrayChildren], outputs));
+    }
+    return result;
+  } else if (type.components.length !== 0) {
+    // struct type
+    const indexedMembers = type.components.map((m) => decode([m], outputs));
+    const namedMembers: { [key: string]: any } = {};
+    type.components.forEach((member, i) => {
+      namedMembers[member.name] = indexedMembers[i];
+    });
+
+    return { ...namedMembers, ...indexedMembers } as Result;
   }
 }
