366080f26dab05c44857ccdcd2282607146a92f8
Swapnil Raj <raj.swapnil99@gmail.com>
2022-10-06 12:27:31 +0000

    Stop from us wasting ungodly amount of time on missing awaits

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,16 @@
 module.exports = {
   env: {
-    es2021: true
+    es2021: true,
   },
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['./tsconfig.json'], // Specify it only for TypeScript files
+  },
   plugins: ['@typescript-eslint'],
-  rules: {"@typescript-eslint/ban-ts-comment": ['error', 'allow-with-description']},
+  rules: {
+    '@typescript-eslint/ban-ts-comment': ['error', 'allow-with-description'],
+    '@typescript-eslint/no-floating-promises': 'error',
+  },
   root: true,
-}
+};
