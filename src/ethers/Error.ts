1221f4396cdca6fc0f5a90ccb3b2b8e258b9baa7
Swapnil Raj <raj.swapnil99@gmail.com>
2022-12-30 19:18:30 +0000

    Add support for revert hardhat chai matchers

--- src/ethers/Error.ts
+++ src/ethers/Error.ts
@@ -1,19 +1,28 @@
+// method id of 'Error(string)'
+const ERROR_STRING_PREFIX = '0x08c379a0';
+
+import { defaultAbiCoder } from '@ethersproject/abi';
+
 export class WarpError extends Error {
   // These properties are needed to place nice with mocha revert matchers
   // This is a carefully crafted hack so check before changing
   name: string;
   errorArgs: string;
   errorName: string;
+  data: string;
   receipt: { revertString: string };
 
   constructor(message: string) {
     super(message);
+    const reason = this.extractRevertReason();
+    // Needed for hardhat chai matchers
+    const encodedReason = defaultAbiCoder.encode(['string'], [reason]).slice('0x'.length);
+
     this.name = 'WarpError';
     this.stack = message;
-
-    const reason = this.extractRevertReason();
     this.errorArgs = reason;
     this.errorName = reason;
+    this.data = `${ERROR_STRING_PREFIX}${encodedReason}`;
     this.receipt = { revertString: reason };
     this.message = `VM Exception while processing transaction: revert ${reason}`;
   }
@@ -24,6 +33,6 @@ export class WarpError extends Error {
     if (matches === null) {
       return '';
     }
-    return matches[1];
+    return matches[1].trim();
   }
 }
